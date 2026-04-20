160e8f2bb09fc6e1e785dd472d9ad37fb37d85b7
Fercisto <dedtroll314@hotmail.com>
2026-04-19 23:06:05 -0600

    Eliminando react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3'
 import App from './App.jsx'
 
@@ -8,10 +7,8 @@ const RECAPTCHA_SITE_KEY = import.meta.env.VITE_RECAPTCHA_SITE_KEY
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <GoogleReCaptchaProvider reCaptchaKey={RECAPTCHA_SITE_KEY}>
-        <App />
-      </GoogleReCaptchaProvider>
-    </BrowserRouter>
+    <GoogleReCaptchaProvider reCaptchaKey={RECAPTCHA_SITE_KEY}>
+      <App />
+    </GoogleReCaptchaProvider>
   </StrictMode>,
 )
