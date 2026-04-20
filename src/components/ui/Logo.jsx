c1cd633970ad3dbedb00c017c5ee10497b49c54c
Fercisto <dedtroll314@hotmail.com>
2026-04-19 23:03:04 -0600

    Cambiando etiqueta en logo y agregando HREF

--- src/components/ui/Logo.jsx
+++ src/components/ui/Logo.jsx
@@ -1,9 +1,7 @@
-import { Link } from "react-router-dom"
-
 export default function Logo() {
   return (
-    <Link to="/" className="text-black font-bold text-xl">
+    <a href="#inicio" className="text-black font-bold text-xl">
       Torres Software Group
-    </Link>
+    </a>
   )
 }
